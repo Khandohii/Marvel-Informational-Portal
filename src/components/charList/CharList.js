ddfdf947e8c60df2431ebe958ac890705a840428
Dmytro <dmitry.khandogy@gmail.com>
2023-08-29 18:55:53 +0200

    Type checking using PropTypes

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,5 +1,7 @@
 
 import { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import './charList.scss';
 import MarvelService from '../../services/MarvelService';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -112,6 +114,10 @@ class CharList extends Component {
             </div>
         )
     }
+}
+
+CharList.propTypes = {
+    onCharSelected: PropTypes.func,
 }
 
 export default CharList;
